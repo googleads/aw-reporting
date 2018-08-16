aca9885518093b33b569e0db0737db450c82a56a
Nick Birnie <nbirnie@google.com>
2018-08-16 18:28:19 +0100

    Exponential backoff prototype

--- aw-reporting-model/src/main/java/com/google/api/ads/adwords/awreporting/model/persistence/sql/SqlReportEntitiesPersister.java
+++ aw-reporting-model/src/main/java/com/google/api/ads/adwords/awreporting/model/persistence/sql/SqlReportEntitiesPersister.java
@@ -15,9 +15,7 @@ package com.google.api.ads.adwords.awreporting.model.persistence.sql;
 
 import com.google.api.ads.adwords.awreporting.model.entities.Report;
 import com.google.api.ads.adwords.awreporting.model.persistence.EntityPersister;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
-import java.time.Duration;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.NonUniqueObjectException;
@@ -30,117 +28,144 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * This is the basic implementation of the persistence layer to communicate with
- * a SQL database. The communication is done using a generic
- * {@link SessionFactory}.
+ * This is the basic implementation of the persistence layer to communicate with a SQL database. The
+ * communication is done using a generic {@link SessionFactory}.
  */
 @Component
 @Qualifier("sqlEntitiesPersister")
-@EnableRetry
 public class SqlReportEntitiesPersister implements EntityPersister {
 
-    private static final Logger logger = LoggerFactory.getLogger(SqlReportEntitiesPersister.class);
-    private static final int BATCH_SIZE = 50;
-    private static final int MAX_RETRIES = 10;
+  private static final Logger logger = LoggerFactory.getLogger(SqlReportEntitiesPersister.class);
 
-    private Duration retryDelay = Duration.ofSeconds(1);
-    private int batchSize = BATCH_SIZE;
-    private SessionFactory sessionFactory;
+  private final SessionFactory sessionFactory;
+  private final Config config;
+
+  /**
+   * Constructor.
+   *
+   * @param sessionFactory the session factory to communicate with the DB.
+   */
+  @Autowired
+  public SqlReportEntitiesPersister(SessionFactory sessionFactory, Config config) {
+    this.sessionFactory =
+        Preconditions.checkNotNull(sessionFactory, "SessionFactory can not be null");
+    this.config = Preconditions.checkNotNull(config, "Config can not be null");
+  }
+
+  /**
+   * Persists all the given entities into the DB configured in the {@code SessionFactory}. Specify
+   * the following system properties backoff.delay
+   */
+  @Override
+  @Transactional
+  @Retryable(
+      value = LockAcquisitionException.class,
+      maxAttemptsExpression = "#{ @systemProperties['retryBackoff'] ?: 10}",
+      backoff =
+          @Backoff(
+              delayExpression = "#{ @systemProperties['retryDelay'] ?: 100}",
+              maxDelayExpression = "#{ @systemProperties['retryMaxDelay'] ?: 10000 }",
+              multiplierExpression = "#{ @systemProperties['retryMultiplier'] ?: 1.5}"))
+  public void persistReportEntities(List<? extends Report> reportEntities) {
+    int batchFlush = 0;
+    Session session = sessionFactory.getCurrentSession();
+
+    for (Report report : reportEntities) {
+      report.setRowId();
+
+      try {
+        session.saveOrUpdate(report);
+      } catch (NonUniqueObjectException ex) {
+        // Github issue 268 & 280
+        //   https://github.com/googleads/aw-reporting/issues/268
+        //   https://github.com/googleads/aw-reporting/issues/280
+        //
+        // Currently we allow specifying report definitions which do not include all primary key
+        // fields. This leads to cryptic hibernate errors without providing a reasonable resolution
+        // strategy.
+        //
+        // This fix explains where to find the list of primary key fields, but does not address the
+        // underlying issue of allowing non-unique rows to be downloaded in the first place.
+        //
+        // Ideally we would guarantee uniqueness of rows without the user having to specify the
+        // PK fields. However, this would be a substantial migration for the AWReporting user base.
+        // Instead, we just log a (hopefully) useful error message.
+        logger.error(
+            "Duplicate row detected. This is most likely because your report definition does not "
+                + "include the primary key fields defined in {}.setRowId(). "
+                + "Please add the missing fields and try again.",
+            report.getClass().getName());
+        throw ex;
+      }
+      batchFlush++;
+
+      if (batchFlush == config.getBatchSize()) {
+        session.flush();
+        session.clear();
+        batchFlush = 0;
+      }
+    }
+  }
+
+  @Override
+  @Transactional
+  public <T> T save(T t) {
+    Session session = sessionFactory.getCurrentSession();
+    session.saveOrUpdate(t);
+    return t;
+  }
+
+  private <T> Criteria createCriteria(Class<T> classT) {
+    Session session = sessionFactory.getCurrentSession();
+    return session.createCriteria(classT);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  @Transactional(readOnly = true)
+  public <T, V> List<T> get(Class<T> classT, String key, V value) {
+    Criteria criteria = createCriteria(classT);
+    criteria.add(Restrictions.eq(key, value));
+    return criteria.list();
+  }
+
+  /** This class is used to specify parameters to the {@link SqlReportEntitiesPersister}. */
+  public static class Config {
+    private static final int DEFAULT_BATCH_SIZE = 50;
+
+    private int batchSize;
+
+    public Config() {
+      this(DEFAULT_BATCH_SIZE);
+    }
+
+    public Config(int batchSize) {
+      this.batchSize = batchSize;
+    }
 
     /**
-     * Constructor.
+     * Retrieve the configured batch size. The default value is defined by DEFAULT_BATCH_SIZE.
      *
-     * @param sessionFactory the session factory to communicate with the DB.
+     * @return the configured batch size.
      */
-    @Autowired
-    public SqlReportEntitiesPersister(SessionFactory sessionFactory) {
-        this.sessionFactory
-                = Preconditions.checkNotNull(sessionFactory, "SessionFactory can not be null");
+    public int getBatchSize() {
+      return batchSize;
     }
 
     /**
-     * Persists all the given entities into the DB configured in the
-     * {@code SessionFactory}.
+     * Configure the batch size. This is the number of operations which will be allowed to
+     * accumulate before flushing the transaction to disk. However it does not force the commit of
+     * the transaction.
+     *
+     * @param requestedBatchSize
      */
-    @Override
-    @Transactional
-    @Retryable(value = LockAcquisitionException.class, maxAttempts = 12, backoff = @Backoff(delay = 100, maxDelay = 300, multiplier = 2.0))
-    public void persistReportEntities(List<? extends Report> reportEntities) {
-        int batchFlush = 0;
-        Session session = sessionFactory.getCurrentSession();
-
-        for (Report report : reportEntities) {
-            report.setRowId();
-
-            try {
-                session.saveOrUpdate(report);
-            } catch (NonUniqueObjectException ex) {
-                // Github issue 268 & 280
-                //   https://github.com/googleads/aw-reporting/issues/268
-                //   https://github.com/googleads/aw-reporting/issues/280
-                //
-                // Currently we allow specifying report definitions which do not include all primary key
-                // fields. This leads to cryptic hibernate errors without providing a reasonable resolution
-                // strategy.
-                //
-                // This fix explains where to find the list of primary key fields, but does not address the
-                // underlying issue of allowing non-unique rows to be downloaded in the first place.
-                //
-                // Ideally we would guarantee uniqueness of rows without the user having to specify the
-                // PK fields. However, this would be a substantial migration for the AWReporting user base.
-                // Instead, we just log a (hopefully) useful error message.
-                logger.error(
-                        "Duplicate row detected. This is most likely because your report definition does not "
-                        + "include the primary key fields defined in {}.setRowId(). "
-                        + "Please add the missing fields and try again.",
-                        report.getClass().getName());
-                throw ex;
-            }
-            batchFlush++;
-
-            if (batchFlush == batchSize) {
-                session.flush();
-                session.clear();
-                batchFlush = 0;
-            }
-        }
+    public void setBatchSize(int requestedBatchSize) {
+      this.batchSize = requestedBatchSize;
     }
-
-    @Override
-    @Transactional
-    public <T> T save(T t) {
-        Session session = sessionFactory.getCurrentSession();
-        session.saveOrUpdate(t);
-        return t;
-    }
-
-    private <T> Criteria createCriteria(Class<T> classT) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createCriteria(classT);
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    @Transactional(readOnly = true)
-    public <T, V> List<T> get(Class<T> classT, String key, V value) {
-        Criteria criteria = createCriteria(classT);
-        criteria.add(Restrictions.eq(key, value));
-        return criteria.list();
-    }
-
-    @VisibleForTesting
-    void setBatchSize(int batchSize) {
-        this.batchSize = batchSize;
-    }
-
-    @VisibleForTesting
-    void setRetryDelay(Duration retryDelay) {
-        this.retryDelay = retryDelay;
-    }
+  }
 }
--- aw-reporting-model/src/test/java/com/google/api/ads/adwords/awreporting/model/persistence/sql/SqlReportEntitiesPersisterTest.java
+++ aw-reporting-model/src/test/java/com/google/api/ads/adwords/awreporting/model/persistence/sql/SqlReportEntitiesPersisterTest.java
@@ -1,147 +1,188 @@
-//// Copyright 2016 Google Inc. All Rights Reserved.
-////
-//// Licensed under the Apache License, Version 2.0 (the "License");
-//// you may not use this file except in compliance with the License.
-//// You may obtain a copy of the License at
-////
-//// http://www.apache.org/licenses/LICENSE-2.0
-////
-//// Unless required by applicable law or agreed to in writing, software
-//// distributed under the License is distributed on an "AS IS" BASIS,
-//// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//// See the License for the specific language governing permissions and
-//// limitations under the License.
+// Copyright 2016 Google Inc. All Rights Reserved.
 //
-//package com.google.api.ads.adwords.awreporting.model.persistence.sql;
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//import static org.junit.Assert.assertThrows;
-//import static org.mockito.Matchers.any;
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//import com.google.api.ads.adwords.awreporting.model.entities.AccountPerformanceReport;
-//import com.google.api.ads.adwords.awreporting.model.entities.DateRangeAndType;
-//import java.time.Duration;
-//import java.util.Arrays;
-//import java.util.List;
-//import org.hibernate.Criteria;
-//import org.hibernate.HibernateException;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.Session;
-//import org.hibernate.exception.LockAcquisitionException;
-//import org.joda.time.LocalDate;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.JUnit4;
-//import org.mockito.InOrder;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//
-///**
-// * Test case for the {@code SqlReportEntitiesPersister} class.
-// */
-//@RunWith(JUnit4.class)
-//public class SqlReportEntitiesPersisterTest {
-//
-//  private SqlReportEntitiesPersister reportEntitiesPersister;
-//
-//  @Mock private Session session;
-//
-//  @Mock private SessionFactory sessionFactory;
-//
-//  @Mock private Criteria criteria;
-//
-//  private InOrder sequence;
-//
-//  @Before
-//  public void setUp() {
-//    MockitoAnnotations.initMocks(this);
-//
-//    Mockito.when(sessionFactory.getCurrentSession()).thenReturn(session);
-//    Mockito.when(session.createCriteria(AccountPerformanceReport.class)).thenReturn(criteria);
-//
-//    sequence = Mockito.inOrder(session);
-//
-//    reportEntitiesPersister = new SqlReportEntitiesPersister(sessionFactory);
-//    reportEntitiesPersister.setRetryDelay(Duration.ZERO);
-//  }
-//
-//  @Test
-//  public void SqlReportEntitiesPersister_persistReportEntities_savesSingleInstance() {
-//    AccountPerformanceReport report = generateReport();
-//
-//    reportEntitiesPersister.persistReportEntities(Arrays.asList(report));
-//
-//    sequence.verify(session).saveOrUpdate(report);
-//  }
-//
-//  @Test
-//  public void SqlReportEntitiesPersister_persistReportEntities_savesInBatches() {
-//    reportEntitiesPersister.setBatchSize(1);
-//
-//    List<AccountPerformanceReport> reports =
-//        Arrays.asList(generateReport(), generateReport(), generateReport());
-//
-//    reportEntitiesPersister.persistReportEntities(reports);
-//
-//    sequence.verify(session).saveOrUpdate(reports.get(0));
-//    sequence.verify(session).flush();
-//    sequence.verify(session).clear();
-//
-//    sequence.verify(session).saveOrUpdate(reports.get(1));
-//    sequence.verify(session).flush();
-//    sequence.verify(session).clear();
-//
-//    sequence.verify(session).saveOrUpdate(reports.get(2));
-//    sequence.verify(session).flush();
-//    sequence.verify(session).clear();
-//  }
-//
-//  @Test
-//  public void SqlReportEntitiesPersister_persistReportEntities_abortsOnSqlException() {
-//    Mockito.doThrow(new HibernateException("")).when(session).saveOrUpdate(any());
-//
-//    assertThrows(
-//        HibernateException.class,
-//        () -> reportEntitiesPersister.persistReportEntities(Arrays.asList(generateReport())));
-//  }
-//
-//  @Test
-//  public void SqlReportEntitiesPersister_persistReportEntities_retriesAfterDeadlockThenFails() {
-//    AccountPerformanceReport report = generateReport();
-//
-//    Mockito.doThrow(new LockAcquisitionException("", null)).when(session).saveOrUpdate(any());
-//
-//    assertThrows(
-//        LockAcquisitionException.class,
-//        () -> reportEntitiesPersister.persistReportEntities(Arrays.asList(report)));
-//
-//    sequence.verify(session, Mockito.times(5)).saveOrUpdate(report);
-//  }
-//
-//  @Test
-//  public void SqlReportEntitiesPersister_persistReportEntities_retriesAfterDeadlockThenSucceeds() {
-//    AccountPerformanceReport report = generateReport();
-//
-//    Mockito.doThrow(new LockAcquisitionException("", null))
-//        .doNothing()
-//        .when(session)
-//        .saveOrUpdate(any());
-//
-//    reportEntitiesPersister.persistReportEntities(Arrays.asList(report));
-//
-//    sequence.verify(session, Mockito.times(2)).saveOrUpdate(report);
-//  }
-//
-//  private AccountPerformanceReport generateReport() {
-//    AccountPerformanceReport report = new AccountPerformanceReport(123L, 456L);
-//    report.setAccountDescriptiveName("testAccount");
-//
-//    LocalDate today = LocalDate.now();
-//    DateRangeAndType dateRange = DateRangeAndType.fromValues(today, today, null);
-//    report.setDateRangeType(dateRange.getTypeStr());
-//    report.setStartDate(dateRange.getStartDateStr());
-//    report.setEndDate(dateRange.getEndDateStr());
-//    return report;
-//  }
-//}
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.api.ads.adwords.awreporting.model.persistence.sql;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+
+import com.google.api.ads.adwords.awreporting.model.entities.AccountPerformanceReport;
+import com.google.api.ads.adwords.awreporting.model.entities.DateRangeAndType;
+import com.google.api.ads.adwords.awreporting.model.persistence.EntityPersister;
+import java.util.Arrays;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.exception.LockAcquisitionException;
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+/**
+ * Test case for the {@code SqlReportEntitiesPersister} class.
+ *
+ * <p>Note: Tests in this class do not use assertThrows as this is not available without updating to
+ * JUnit5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    classes = {SqlReportEntitiesPersisterTest.Config.class},
+    loader = AnnotationConfigContextLoader.class)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class SqlReportEntitiesPersisterTest {
+
+  @Autowired
+  @Qualifier("sqlEntitiesPersister")
+  private EntityPersister sqlEntitiesPersister;
+
+  @Autowired private Session session;
+
+  @Autowired private SessionFactory sessionFactory;
+
+  @Autowired private SqlReportEntitiesPersister.Config config;
+
+  @Mock private Criteria criteria;
+
+  private InOrder sequence;
+
+  @Configuration
+  @ComponentScan(
+      basePackageClasses = {
+        com.google.api.ads.adwords.awreporting.model.persistence.sql.SqlReportEntitiesPersister
+            .class
+      })
+  @EnableRetry
+  public static class Config {
+
+    @Bean
+    public SessionFactory sessionFactory() {
+      return Mockito.mock(SessionFactory.class);
+    }
+
+    @Bean
+    public Session session() {
+      return Mockito.mock(Session.class);
+    }
+
+    @Bean
+    SqlReportEntitiesPersister.Config config() {
+      return new SqlReportEntitiesPersister.Config();
+    }
+  }
+
+  @Before
+  public void setUp() {
+    Mockito.when(sessionFactory.getCurrentSession()).thenReturn(session);
+    Mockito.when(session.createCriteria(AccountPerformanceReport.class)).thenReturn(criteria);
+
+    sequence = Mockito.inOrder(session);
+  }
+
+  @Test
+  public void persistReportEntities_savesSingleInstance() {
+    AccountPerformanceReport report = generateReport(1);
+
+    sqlEntitiesPersister.persistReportEntities(Arrays.asList(report));
+
+    sequence.verify(session).saveOrUpdate(report);
+  }
+
+  @Test
+  public void persistReportEntities_savesInBatches() {
+    config.setBatchSize(2);
+
+    List<AccountPerformanceReport> reports =
+        Arrays.asList(generateReport(1), generateReport(2), generateReport(3));
+
+    sqlEntitiesPersister.persistReportEntities(reports);
+
+    sequence.verify(session).saveOrUpdate(reports.get(0));
+    sequence.verify(session).saveOrUpdate(reports.get(1));
+    sequence.verify(session).flush();
+    sequence.verify(session).clear();
+
+    sequence.verify(session).saveOrUpdate(reports.get(2));
+  }
+
+  @Test
+  public void persistReportEntities_abortsOnSqlException() {
+    Mockito.doThrow(new HibernateException("")).when(session).saveOrUpdate(any());
+
+    try {
+      sqlEntitiesPersister.persistReportEntities(Arrays.asList(generateReport(1)));
+      fail();
+    } catch (HibernateException ex) {
+      // expected
+    }
+  }
+
+  @Test
+  public void persistReportEntities_retriesAfterDeadlockThenFails() {
+    AccountPerformanceReport report = generateReport(1);
+
+    Mockito.doThrow(new LockAcquisitionException("", null)).when(session).saveOrUpdate(any());
+
+    try {
+      sqlEntitiesPersister.persistReportEntities(Arrays.asList(report));
+      fail();
+    } catch (LockAcquisitionException ex) {
+      // expected
+    }
+
+    sequence.verify(session, Mockito.times(10)).saveOrUpdate(report);
+  }
+
+  @Test
+  public void persistReportEntities_retriesAfterDeadlockThenSucceeds() {
+    AccountPerformanceReport report = generateReport(1);
+
+    Mockito.doThrow(new LockAcquisitionException("", null))
+        .doNothing()
+        .when(session)
+        .saveOrUpdate(any());
+
+    sqlEntitiesPersister.persistReportEntities(Arrays.asList(report));
+
+    sequence.verify(session, Mockito.times(2)).saveOrUpdate(report);
+  }
+
+  private AccountPerformanceReport generateReport(long accountId) {
+    AccountPerformanceReport report = new AccountPerformanceReport(123L, accountId);
+    report.setAccountDescriptiveName("testAccount");
+
+    LocalDate today = LocalDate.now();
+    DateRangeAndType dateRange = DateRangeAndType.fromValues(today, today, null);
+    report.setDateRangeType(dateRange.getTypeStr());
+    report.setStartDate(dateRange.getStartDateStr());
+    report.setEndDate(dateRange.getEndDateStr());
+    return report;
+  }
+}
